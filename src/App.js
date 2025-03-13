a802504202827f279f4645d5a4fd0e2bf6787687
Gene <garellano@csus.edu>
2025-03-12 23:47:34 -0700

    boiler plate

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import Layout from "./components/layout"; 
+
+function App() {
+  return (
+    <Layout title="Page Title">
+      <span>Content</span>
+    </Layout>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"], // Scans all relevant files in /src
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
