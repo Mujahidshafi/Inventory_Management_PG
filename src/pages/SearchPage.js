555ff5eb42c836b19901be96a5403a88e311e0fd
Subhan-4 <slalzai@csus.edu>
2025-03-16 17:05:27 -0700

    Update SearchPage.js

--- src/pages/SearchPage.js
+++ src/pages/SearchPage.js
@@ -1,1 +1,13 @@
+import React from "react";
+import Layout from "../components/layout"; // Adjust if needed
 
+function SearchPage() {
+  return (
+    <Layout title="Search Page">
+      <h1>Search Page</h1>
+      <p>This is the search page.</p>
+    </Layout>
+  );
+}
+
+export default SearchPage;
