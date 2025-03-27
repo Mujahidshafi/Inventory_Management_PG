089f1ee681db315f93c23928d169047a523a08ff
p-maina <puritymaina@csus.edu>
2025-03-27 15:53:17 -0700

    storage menu buttons with routes (draft)

--- src/pages/StorageDashboard.js
+++ src/pages/StorageDashboard.js
@@ -1,11 +1,17 @@
 import React from "react";
 import "../App.css";
-import Layout from "../components/layout"; 
+import Layout from "../components/layout";
+import { Link } from "react-router-dom";
 
 function StorageDashboard() {
   return (
     <Layout title="Storage Dashboard">
-      <span>Storage Dashboard Page Content</span>
+      <Link to="/fieldRunStorage" className="bg-[#5D1214] text-white px-6 py-4 rounded-[15px] text-lg font-semibold 
+      flex-1 text-center hover:bg-[#390B0EFF] transition-all duration-300"> Field Run Storage </Link>
+      <Link to="/cleanStorage" className="bg-[#5D1214] text-white px-6 py-4 rounded-[15px] text-lg font-semibold 
+      flex-1 text-center hover:bg-[#390B0EFF] transition-all duration-300"> Clean Storage </Link>
+      <Link to="/screeningStorage" className="bg-[#5D1214] text-white px-6 py-4 rounded-[15px] text-lg font-semibold 
+      flex-1 text-center hover:bg-[#390B0EFF] transition-all duration-300"> Screening Storage </Link>
     </Layout>
   );
 }
