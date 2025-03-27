06341a327fd85ec2c3af41f5c8703ab5b558ea08
p-maina <puritymaina@csus.edu>
2025-03-27 16:21:07 -0700

    #157 updated button styling

--- src/pages/StorageDashboard.js
+++ src/pages/StorageDashboard.js
@@ -5,14 +5,16 @@ import { Link } from "react-router-dom";
 
 function StorageDashboard() {
   return (
-    <Layout title="Storage Dashboard">
-      <Link to="/fieldRunStorage" className="bg-[#5D1214] text-white px-6 py-4 rounded-[15px] text-lg font-semibold 
-      flex-1 text-center hover:bg-[#390B0EFF] transition-all duration-300"> Field Run Storage </Link>
-      <Link to="/cleanStorage" className="bg-[#5D1214] text-white px-6 py-4 rounded-[15px] text-lg font-semibold 
-      flex-1 text-center hover:bg-[#390B0EFF] transition-all duration-300"> Clean Storage </Link>
-      <Link to="/screeningStorage" className="bg-[#5D1214] text-white px-6 py-4 rounded-[15px] text-lg font-semibold 
-      flex-1 text-center hover:bg-[#390B0EFF] transition-all duration-300"> Screening Storage </Link>
-    </Layout>
+      <Layout title="Storage Dashboard">
+        <div className= "grid grid-cols-3 gap-20">
+        <Link to="/fieldRunStorage" className="bg-[#5D1214] text-white px-6 py-6 rounded-[15px] text-lg font-semibold 
+         text-center hover:bg-[#390B0EFF] transition-all duration-300"> Field Run Storage </Link>
+        <Link to="/cleanStorage" className="bg-[#5D1214] text-white px-6 py-6 rounded-[15px] text-lg font-semibold 
+         text-center hover:bg-[#390B0EFF] transition-all duration-300"> Clean Storage </Link>
+        <Link to="/screeningStorage" className="bg-[#5D1214] text-white px-6 py-6 rounded-[15px] text-lg font-semibold 
+         text-center hover:bg-[#390B0EFF] transition-all duration-300"> Screening Storage </Link>
+         </div>
+      </Layout>
   );
 }
 
