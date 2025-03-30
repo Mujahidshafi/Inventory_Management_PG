e65d7c4ad1a17bae058d515b0a5867ecc1537346
genearellano <145499451+genearellano@users.noreply.github.com>
2025-03-30 12:17:38 -0700

    Update newFieldRun
    Add button

--- src/pages/newFieldRun.js
+++ src/pages/newFieldRun.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import Layout from "../components/layout";
+import Button from "../components/button";
 
 function NewFieldRun() {
   const [fields, setFields] = useState({
@@ -17,37 +18,29 @@ function NewFieldRun() {
 
   return (
     <Layout title="New Field Run">
-      <div className="h-[80vh] w-full px-8 flex flex-col justify-between">
-
-        <div className="flex h-[20vh]">
-          {[1, 2, 3].map((i) => (
-            <div key={i} className="flex flex-col flex-1">
+      <div className="w-full px-8 flex flex-col items-center">
+        
+        {/* Input fields container */}
+        <div className="flex flex-wrap justify-between w-full max-w-5xl mb-8">
+          {[1, 2, 3, 4, 5, 6].map((i) => (
+            <div key={i} className="flex flex-col w-[30%] min-w-[200px] mb-4">
               <span className="mb-1 font-medium text-gray-700">Field Lot Number</span>
               <input
                 type="text"
                 value={fields[`field${i}`]}
                 onChange={(e) => handleChange(e, `field${i}`)}
                 placeholder={`Enter value ${i}`}
-                className="w-[50vh] px-2 py-1 border border-gray-400 rounded-md shadow focus:outline-none focus:ring-2 focus:ring-blue-400"
+                className="px-2 py-1 border border-gray-400 rounded-md shadow focus:outline-none focus:ring-2 focus:ring-blue-400"
               />
             </div>
           ))}
         </div>
-
-        <div className="flex h-[60vh]">
-          {[4, 5, 6].map((i) => (
-            <div key={i} className="flex flex-col flex-1">
-              <span className="mb-1 font-medium text-gray-700">Field Lot Number</span>
-              <input
-                type="text"
-                value={fields[`field${i}`]}
-                onChange={(e) => handleChange(e, `field${i}`)}
-                placeholder={`Enter value ${i}`}
-                className="w-[50vh] px-2 py-1 border border-gray-400 rounded-md shadow focus:outline-none focus:ring-2 focus:ring-blue-400"
-              />
-            </div>
-          ))}
+  
+        {/* Centered Button */}
+        <div className="flex justify-center">
+          <Button label="Save" color="red" />
         </div>
+  
       </div>
     </Layout>
   );
